58035a7a4ec194eb89f76f31a2ebedf2182fe3f8
Henry Allen-Tilford <htilford@gmail.com>
2016-09-20 09:57:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,51 +1,38 @@
-var express = require('express');
-var app = express();
-var redis = require("redis");
+const express = require('express')
+const redis = require('redis')
 
-var r;
-var options = {
-	max_attempts : 1
-};
-var redisConnected = false;
+const app = express()
+const options = { max_attempts: 1 }
+const r = redis.createClient(null, 'redis', options)
 
-if( process.env.REDIS_PORT && process.env.REDIS_ENV_REDIS_PASS ){
-	options.auth_pass = process.env.REDIS_ENV_REDIS_PASS;
+let redisConnected = false;
 
-	var port = process.env.REDIS_PORT_6379_TCP_PORT;
-	var host = process.env.REDIS_PORT_6379_TCP_ADDR;
+r.on('error', (err) => {
+  console.log('Error on redis', err)
+})
 
-	r = redis.createClient(port, host, options);
-}else{
-	r = redis.createClient(null, null, options);
-}
+r.on('connect', () => {
+  redisConnected = true
+})
 
-r.on('error', function(err){
-	console.log('Error on redis', err);
-});
-
-r.on('connect', function(){
-	redisConnected = true;
-});
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	if(!redisConnected){
-		return res.send('No conection to redis');
+		return res.send('No connection to redis')
 	}
 
-	r.incr('counter', function (err, data) {
-		if(err){return res.status(500).send(err);}
-
-		res.send('Visits '+ data);
-	});
-});
+	r.incr('counter', (err, count) => {
+		if(err){
+		  return res.status(500).send(err)
+		}
+		res.send(`Visits ${count}`)
+	})
+})
 
 app.get('/env', function (req, res) {
-	res.json( process.env );
-});
+	res.json(process.env)
+})
 
-var server = app.listen(process.env.PORT || 3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-});
+const server = app.listen(80, function () {
+	const { host, port } = server.address()
+	console.log(`Example app listening at http://${host}:${port}`)
+})
