5142900a109ab740aca2f9c978551e51b8981024
Henry Allen-Tilford <htilford@gmail.com>
2016-09-20 09:59:30 -0700

    Delete test.js

--- test/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('env', function () {
- it('should have REDIS_PORT_6379_TCP_PORT equal to 6379', function (done) {
-     var port = process.env.REDIS_PORT_6379_TCP_PORT;
-     port.should.equal("6379");
-     done();
- });
-});
